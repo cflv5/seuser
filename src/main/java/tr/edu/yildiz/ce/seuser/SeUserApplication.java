8ca87d641477b65702c7a74b4fea231f0c77933b
cflv5 <bedirhanakcay@icloud.com>
2022-05-30 20:25:33 +0300

    initial commit

---
+++ src/main/java/tr/edu/yildiz/ce/seuser/SeUserApplication.java
@@ -0,0 +1,13 @@
+package tr.edu.yildiz.ce.seuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/edu/yildiz/ce/seuser/SeUserApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.edu.yildiz.ce.seuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
