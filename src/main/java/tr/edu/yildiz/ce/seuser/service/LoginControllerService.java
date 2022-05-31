cef0013b2fff78ac6eb155d891a5f17d0cb9fd31
cflv5 <bedirhanakcay@icloud.com>
2022-05-31 16:21:54 +0300

    feat: add logic for registring a user

--- src/main/java/tr/edu/yildiz/ce/seuser/service/LoginControllerService.java
+++ src/main/java/tr/edu/yildiz/ce/seuser/service/LoginControllerService.java
@@ -2,19 +2,26 @@ package tr.edu.yildiz.ce.seuser.service;
 
 import org.springframework.stereotype.Service;
 
+import tr.edu.yildiz.ce.se.base.domain.HeaderMessage;
 import tr.edu.yildiz.ce.se.base.domain.ResponseHeader;
 import tr.edu.yildiz.ce.seuser.domain.dto.LoginControllerRequest;
 import tr.edu.yildiz.ce.seuser.domain.dto.LoginControllerResponse;
+import tr.edu.yildiz.ce.seuser.domain.dto.RegisterControllerRequest;
+import tr.edu.yildiz.ce.seuser.domain.dto.RegisterControllerResponse;
 import tr.edu.yildiz.ce.seuser.domain.entity.User;
 import tr.edu.yildiz.ce.seuser.exception.AuthenticationException;
+import tr.edu.yildiz.ce.seuser.service.repository.ProfileRepositoryService;
 import tr.edu.yildiz.ce.seuser.service.repository.UserRepositoryService;
 
 @Service
 public class LoginControllerService {
     private final UserRepositoryService userRepositoryService;
+    private final ProfileRepositoryService profileRepositoryService;
 
-    public LoginControllerService(UserRepositoryService userRepositoryService) {
+    public LoginControllerService(UserRepositoryService userRepositoryService,
+            ProfileRepositoryService profileRepositoryService) {
         this.userRepositoryService = userRepositoryService;
+        this.profileRepositoryService = profileRepositoryService;
     }
 
     public LoginControllerResponse authenticate(LoginControllerRequest request) {
@@ -28,6 +35,28 @@ public class LoginControllerService {
         }
 
         return new LoginControllerResponse(ResponseHeader.success(), user.getId());
+    }
+
+    public RegisterControllerResponse register(RegisterControllerRequest request) {
+        var email = request.getUser().getEmail();
+
+        if (userRepositoryService.getUserByUsername(email).isPresent()) {
+            return new RegisterControllerResponse(ResponseHeader.fail(
+                    HeaderMessage.Builder
+                            .create()
+                            .code(302)
+                            .text(String.format("User with email %s already exists", email))
+                            .build()),
+                    "");
+        }
+
+        var profile = profileRepositoryService.createAndSaveProfile(request.getUser());
+
+        var user = User.of(request.getUser());
+        user.setProfile(profile);
+        user = userRepositoryService.saveUser(user);
+
+        return new RegisterControllerResponse(ResponseHeader.success(), user.getId());
     }
 
 }
