f999da502f2ec85b13b9613a15d9fef916e0c00f
cflv5 <bedirhanakcay@icloud.com>
2022-05-31 16:22:10 +0300

    feat: implement an end point for registring a user

--- src/main/java/tr/edu/yildiz/ce/seuser/api/LoginController.java
+++ src/main/java/tr/edu/yildiz/ce/seuser/api/LoginController.java
@@ -1,5 +1,7 @@
 package tr.edu.yildiz.ce.seuser.api;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import tr.edu.yildiz.ce.seuser.domain.dto.LoginControllerRequest;
 import tr.edu.yildiz.ce.seuser.domain.dto.LoginControllerResponse;
+import tr.edu.yildiz.ce.seuser.domain.dto.RegisterControllerRequest;
+import tr.edu.yildiz.ce.seuser.domain.dto.RegisterControllerResponse;
 import tr.edu.yildiz.ce.seuser.service.LoginControllerService;
 
 @RestController
@@ -25,13 +29,25 @@ public class LoginController {
     }
 
     @PostMapping(value = "/login")
-    public ResponseEntity<LoginControllerResponse> getMethodName(@RequestBody LoginControllerRequest request) {
+    public ResponseEntity<LoginControllerResponse> login(@RequestBody @Valid LoginControllerRequest request) {
         LoginControllerResponse response;
         try {
             response = loginControllerService.authenticate(request);
         } catch (Exception e) {
             LOGGER.error("Exception occured while authentication", e);
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<RegisterControllerResponse> register(@RequestBody @Valid RegisterControllerRequest request) {
+        RegisterControllerResponse response;
+        try {
+            response = loginControllerService.register(request);
+        } catch (Exception e) {
+            LOGGER.error("Exception occured while authentication", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
         return ResponseEntity.ok(response);
     }
