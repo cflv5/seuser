068a0627f5ccca763d654711f2538a18cf05468b
cflv5 <bedirhanakcay@icloud.com>
2022-06-07 15:41:35 +0300

    feat: create repository for profile

--- src/main/java/tr/edu/yildiz/ce/seuser/repository/ProfileRepository.java
+++ src/main/java/tr/edu/yildiz/ce/seuser/repository/ProfileRepository.java
@@ -1,9 +1,11 @@
 package tr.edu.yildiz.ce.seuser.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import tr.edu.yildiz.ce.seuser.domain.entity.Profile;
 
 public interface ProfileRepository extends JpaRepository<Profile, Integer> {
-    
+    Optional<Profile> getProfileByTenantId(String tenantId);
 }
