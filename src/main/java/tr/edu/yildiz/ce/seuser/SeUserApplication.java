72221ba00d1bc5435a45b671a8b06213aee82e14
cflv5 <bedirhanakcay@icloud.com>
2022-06-07 15:42:29 +0300

    feat: scan base project to include releated components

--- src/main/java/tr/edu/yildiz/ce/seuser/SeUserApplication.java
+++ src/main/java/tr/edu/yildiz/ce/seuser/SeUserApplication.java
@@ -2,8 +2,10 @@ package tr.edu.yildiz.ce.seuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "tr.edu.yildiz.ce.se.base", "tr.edu.yildiz.ce.seuser" })
 public class SeUserApplication {
 
 	public static void main(String[] args) {
