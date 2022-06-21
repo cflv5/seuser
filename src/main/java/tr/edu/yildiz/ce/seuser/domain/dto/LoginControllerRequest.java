77afbcce0712d81167fa44bea2fc6717ddee36af
cflv5 <bedirhanakcay@icloud.com>
2022-06-21 19:53:03 +0300

    chore: remove request header

--- src/main/java/tr/edu/yildiz/ce/seuser/domain/dto/LoginControllerRequest.java
+++ src/main/java/tr/edu/yildiz/ce/seuser/domain/dto/LoginControllerRequest.java
@@ -1,13 +1,8 @@
 package tr.edu.yildiz.ce.seuser.domain.dto;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-import tr.edu.yildiz.ce.se.base.domain.RequestHeader;
 
 public class LoginControllerRequest {
-    @NotNull
-    private RequestHeader header;
     @NotBlank
     private String email;
     @NotBlank
@@ -15,14 +10,6 @@ public class LoginControllerRequest {
 
     public LoginControllerRequest() {
         super();
-    }
-
-    public RequestHeader getHeader() {
-        return header;
-    }
-
-    public void setHeader(RequestHeader header) {
-        this.header = header;
     }
 
     public String getEmail() {
